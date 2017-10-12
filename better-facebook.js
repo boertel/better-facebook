51c52896b4c601c4c0db8d517cc6754a00db130b
boertel <benjamin.oertel@gmail.com>
2017-10-12 16:43:20 -0700

    better

--- better-facebook.js
+++ better-facebook.js
@@ -9,6 +9,7 @@ var style = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
     zIndex: 999999,
     color: '#fff',
     fontSize: '80px'
@@ -18,9 +19,32 @@ for (var key in style) {
     overlay.style[key] = style[key];
 }
 
-overlay.innerHTML = 'Are you really bored?';
-overlay.onclick = function() {
-    document.body.removeChild(overlay);
-};
 
-document.body.appendChild(overlay);
+var KEY = 'lastVisit';
+
+var previous = +localStorage.getItem(KEY);
+var now = new Date().getTime();
+
+var LIMIT = 5 * 60 * 1000;
+
+var minutes = parseInt((((LIMIT + previous) - now) / 1000 / 60), 10);
+var text = minutes >= 0 ? 'You have to wait' : 'Good job! You have being on Facebook for';
+var wait = '<br><em><small style="font-size: 0.4em">' + text + ' ' + Math.abs(minutes) + ' ' + (Math.abs(minutes) === 1 ? 'minute' : 'minutes') + '.</small></em>'
+overlay.innerHTML = 'Are you really bored?' + wait;
+
+if (now - previous > LIMIT) {
+    overlay.onclick = function() {
+        document.body.removeChild(overlay);
+    };
+}
+
+var monkey = window.onbeforeunload;
+window.onbeforeunload = function (evt) {
+    monkey && monkey(evt);
+    localStorage.setItem(KEY, new Date().getTime());
+}
+
+var isFromFacebook = document.referrer.startsWith('https://www.facebook.com')
+if (!isFromFacebook) {
+    document.body.appendChild(overlay);
+}
